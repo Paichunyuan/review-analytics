ad12b39d9015aeffe6b644daaccaffcb22c609d2
Paichunyuan <72922776+Paichunyuan@users.noreply.github.com>
2020-12-10 20:49:16 +0800

    New

--- review-analytics.py
+++ review-analytics.py
@@ -4,11 +4,54 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count = count + 1
-		if count % 10000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
+
+print(data[0])
 
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('留言的平均為', sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print ('一共有', len(good), '筆留言提到good')
+
+#文字計數
+wc = {} #word_count
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] = wc[word] + 1
+		else:
+			wc[word] = 1
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word = input('請問你想查甚麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現', wc[word], '次')
+	else:
+		print('這個字沒有出現過喔!')
+
+print('感謝使用')
